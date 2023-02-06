0eedc854d70e9fb26e3946b503aa625d341e25f2
norrico31 <norricobiason31@gmail.com>
2023-02-06 15:21:53 +0800

    another example for routing

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/posts', function () {
+//     return response()->json([
+//         'posts' => [
+//             'title' => 'Post One',
+//             'body' => 'Message Body 1'
+//         ],
+//         [
+//             'title' => 'Post Two',
+//             'body' => 'Message Body 2'
+//         ]
+//     ]);
+// });
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return response('<h1>Hello World</h1>', 200);
-});
+// Route::get('/hello', function () {
+//     return response('<h1>Hello World</h1>', 200);
+// });
 
-Route::get('/post/{id}', function ($id) {
-    // dd($id);
-    return response('Post: ' . $id);
-})->where('id', '[0-9]+');
+// Route::get('/post/{id}', function ($id) {
+//     // dd($id);
+//     return response('Post: ' . $id);
+// })->where('id', '[0-9]+');
 
-Route::get('/search', function (Request $request) {
-    dd($request->name . ' ' . $request->lastname);
-    // return response($request->name . ' ' . $request->lastname, 200);
-});
+// Route::get('/search', function (Request $request) {
+//     dd($request->name . ' ' . $request->lastname);
+//     // return response($request->name . ' ' . $request->lastname, 200);
+// });
